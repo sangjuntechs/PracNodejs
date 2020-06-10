762e0945bb716c9a186d48ad1c44f44c59843bcc
sangjun <juntech0421@github.com>
2020-06-10 16:22:34 +0900

    mongodb connect

--- README.md
+++ README.md
@@ -3,3 +3,18 @@ Node.js의 연습을 위한 repo.
 
 
 ## node와 express.js의 기본 작동방식 확인
+
+## mongodb 와 프로젝트 연결
+
+#### 콘솔로그로 몽고디비와 프로젝트가 연결 되었는지 확인
+```ts
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://sangjun:tkdwns12@practice-zi0wv.mongodb.net/practice?retryWrites=true&w=majority'
+    ,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }).then(() => console.log('mongodb Connected..'))
+    .catch(err => console.log(err))
+```
--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://sangjun:tkdwns12@practice-zi0wv.mongodb.net/practice?retryWrites=true&w=majority'
+    ,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }).then(() => console.log('mongodb Connected..'))
+    .catch(err => console.log(err))
+
+
+
 app.get('/', (req, res) => res.send('Hello Express.js!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
